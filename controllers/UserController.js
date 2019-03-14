0f23b10e0a3c17b1408011f5d3b0f3e41a11c7df
Renato Babata <babatarenato@gmail.com>
2019-03-14 19:36:28 -0300

    14/03 home

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,3 +1,6 @@
+//date.getTime() -> pegar o timestamp
+//metodo estatico
+
 class UserController{
 
     constructor(formId,tableId){
@@ -7,17 +10,23 @@ class UserController{
         this.onSubmit();
     }
 
-    onSubmit(){
+    onSubmit(){ 
             
         this.formEl.addEventListener("submit", event => {
         event.preventDefault();
-        
+
+        let btn = document.querySelector("[type = submit]");
+
+        btn.disabled = true;
+
         let values = this.getValues();
 
         this.getPhoto().then(
              (content)=>{
                 values.photo = content;
                 this.addLine(values);
+                this.formEl.reset();
+                btn.disabled = false;
             }, (e)=>{ 
                 console.error(e);
             });
@@ -46,7 +55,11 @@ class UserController{
                 reject(e);
             }
 
-            fileReader.readAsDataURL(file);
+            if(file){
+                fileReader.readAsDataURL(file);
+            }else{
+                resolve('dist/img/boxed-bg.jpg');
+            }
 
         });
 
@@ -56,18 +69,29 @@ class UserController{
     getValues(){
 
         let user = {};
+        let isValid = true;
 
             [...this.formEl.elements].forEach(function(field,index){
+
+            if(['email','password'].indexOf(field.name) > -1 && !field.value){
+                field.parentElement.classList.add('has-error');
+                isValid = false;
+            }
 
             if(field.name == "gender"){
                 if(field.checked){
                     user[field.name] = field.value;
                 }
+            }else if(field.name == "admin"){
+                user[field.name] = field.checked;
             }else{
                 user[field.name] = field.value;
             }
         });
     
+        if(isValid){
+            return false;
+        }
         return new User(
             user.name,
             user.gender,
@@ -78,27 +102,28 @@ class UserController{
             user.photo,
             user.admin  
         );
+    
+       
 
     }
 
     addLine(dataUser){
     
-    
-        this.tableEl.innerHTML = `
-        <tr>
+        let tr = document.createElement('tr');
+
+        tr.innerHTML = `
             <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
             <td>${dataUser.name}</td>
             <td>${dataUser.email}</td>
-            <td>${dataUser.admin}</td>
-            <td>${dataUser.birth}</td>
+            <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
+            <td>${(Utils.dateFormat(dataUser.register))}</td>
             <td>
             <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
             <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
             </td>
-        </tr>
-    
         `;
-    
+        
+        this.tableEl.appendChild(tr);
     }
     
 }
